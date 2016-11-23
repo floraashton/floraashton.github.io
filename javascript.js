d5ee8cbb0aa276d2b6800db4be91b824b1ada036
lauraeyles <lauraeyles@Lauras-MacBook-Air.local>
2016-11-23 17:20:54 +0000

    vinales info

--- javascript.js
+++ javascript.js
@@ -3,6 +3,15 @@ $(document).ready(function(){
 
 $('#Havana h1').click(function() {$('#Havana h2').fadeIn()});
 
-$('#Havana h2').click(function() {$('#Havana p').fadeIn()});
+$('#Havana h1').click(function() {$('#Havana p').fadeIn()});
+
+$('#Vinales h1').click(function() {$('#Vinales h2').fadeIn()});
+
+$('#Vinales h1').click(function() {$('#Vinales p').fadeIn()});
+
+$('#Trinidad h1').click(function() {$('#Trinidad h2').fadeIn()});
+
+$('#Trinidad h1').click(function() {$('#Trinidad p').fadeIn()});
+
 
 })

c3a315281faa4311ae41a1093821e1469fa27a90
floraashton <floraashton@hotmail.co.uk>
2016-11-23 19:31:33 +0000

    see less button havana

--- javascript.js
+++ javascript.js
@@ -5,9 +5,19 @@ $('#Havana h1').click(function() {$('#Havana h2').fadeIn()});
 
 $('#Havana h1').click(function() {$('#Havana p').fadeIn()});
 
+$('#Havana h1').click(function() {$('h3').fadeIn()});
+
+$('h3').click(function() {$('#Havana p').hide()});
+
+$('h3').click(function() {$('#Havana h2').hide()});
+
+$('h3').click(function() {$('h3').hide()});
+
+
 $('#Vinales h1').click(function() {$('#Vinales h2').fadeIn()});
 
 $('#Vinales h1').click(function() {$('#Vinales p').fadeIn()});
+
 
 $('#Trinidad h1').click(function() {$('#Trinidad h2').fadeIn()});
 

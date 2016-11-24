05095b78b47c3c3fb99938b073c49aab435a50ee
lauraeyles <lauraeyles@Lauras-MacBook-Air-2.local>
2016-11-24 12:03:22 +0000

    Trinidad info and bold words

--- javascript.js
+++ javascript.js
@@ -18,10 +18,26 @@ $('#Vinales h1').click(function() {$('#Vinales h2').fadeIn()});
 
 $('#Vinales h1').click(function() {$('#Vinales p').fadeIn()});
 
+$('#Vinales h1').click(function() {$('h4').fadeIn()});
+
+$('h4').click(function() {$('#Vinales p').fadeOut()});
+
+$('h4').click(function() {$('#Vinales h2').fadeOut()});
+
+$('h4').click(function() {$('h4').fadeOut()});
+
 
 $('#Trinidad h1').click(function() {$('#Trinidad h2').fadeIn()});
 
 $('#Trinidad h1').click(function() {$('#Trinidad p').fadeIn()});
+
+$('#Trinidad h1').click(function() {$('h5').fadeIn()});
+
+$('h5').click(function() {$('#Trinidad p').fadeOut()});
+
+$('h5').click(function() {$('#Trinidad h2').fadeOut()});
+
+$('h5').click(function() {$('h5').fadeOut()});
 
 
 })

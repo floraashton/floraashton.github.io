2eb32d3a53e7549606699c0dadb5e758b4e4c435
floraashton <floraashton@hotmail.co.uk>
2016-11-25 16:14:02 +0000

    more see less buttons

--- javascript.js
+++ javascript.js
@@ -1,27 +1,53 @@
 $(document).ready(function(){
 
 
+//Havana//
+
 $('#Havana h1').click(function() {$('#Havana h2').fadeIn()});
 
 $('#Havana h1').click(function() {$('#Havana p').fadeIn()});
 
-$('#Havana h1').click(function() {$('h3').fadeIn()});
+$('#Havana h1').click(function() {$('#Havana h3').fadeIn()});
 
-$('h3').click(function() {$('#Havana p').fadeOut()});
 
-$('h3').click(function() {$('#Havana h2').fadeOut()});
+$('#Havana h3').click(function() {$('#Havana p').fadeOut()});
 
-$('h3').click(function() {$('h3').fadeOut()});
+$('#Havana h3').click(function() {$('#Havana h2').fadeOut()});
 
+$('#Havana h3').click(function() {$('#Havana h3').fadeOut()});
+
+
+
+//Vinales//
 
 $('#Vinales h1').click(function() {$('#Vinales h2').fadeIn()});
 
 $('#Vinales h1').click(function() {$('#Vinales p').fadeIn()});
+
+$('#Vinales h1').click(function() {$('#Vinales h3').fadeIn()});
+
+
+$('#Vinales h3').click(function() {$('#Vinales p').fadeOut()});
+
+$('#Vinales h3').click(function() {$('#Vinales h2').fadeOut()});
+
+$('#Vinales h3').click(function() {$('#Vinales h3').fadeOut()});
+
+
+//Trinidad//
 
 
 $('#Trinidad h1').click(function() {$('#Trinidad h2').fadeIn()});
 
 $('#Trinidad h1').click(function() {$('#Trinidad p').fadeIn()});
 
+$('#Trinidad h1').click(function() {$('#Trinidad h3').fadeIn()});
+
+
+$('#Trinidad h3').click(function() {$('#Trinidad p').fadeOut()});
+
+$('#Trinidad h3').click(function() {$('#Trinidad h2').fadeOut()});
+
+$('#Trinidad h3').click(function() {$('#Trinidad h3').fadeOut()});
 
 })
